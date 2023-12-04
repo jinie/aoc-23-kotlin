ee55e82839955389e800040cb6761ba4192de221
Jimmy Selgen Nielsen <jyn@bankdata.dk>
2023-12-04 11:33:53 +0100

    Optimize Day04 input parsing

--- src/Day04.kt
+++ src/Day04.kt
@@ -4,19 +4,16 @@ fun main() {
         val ret = mutableMapOf<Int,Pair<Set<Int>, Set<Int>>>()
         input.forEach {line ->
             var (card, numberSource) = line.split(":")
-            card = card.removePrefix("Card ")
-            val (win,stake) = numberSource.split("|")
-            val winners = win.split(" ").mapNotNull { if(it.isNotEmpty()) it.strip().toInt() else null }.toSet()
-            val stakes = stake.split(" ").mapNotNull { if(it.isNotEmpty()) it.strip().toInt() else null }.toSet()
-            ret[card.strip().toInt()] = Pair(winners, stakes)
+            card = card.removePrefix("Card ").strip()
+            val (winners,stakes) = numberSource.split("|").map { it.split(" ").mapNotNull { if(it.isNotEmpty()) it.strip().toInt() else null }.toSet() }
+            ret[card.toInt()] = Pair(winners, stakes)
         }
         return ret
     }
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: Map<Int,Pair<Set<Int>, Set<Int>>>): Int {
         var ret = 0
-        val inp = parseInput(input)
-        inp.forEach { (_, data) ->
+        input.forEach { (_, data) ->
             val common = data.first.intersect(data.second)
             var tret = 1
             if(common.isNotEmpty()) {
@@ -28,11 +25,10 @@ fun main() {
         return ret
     }
 
-    fun part2(input: List<String>): Int {
-        val inp = parseInput(input)
+    fun part2(input: Map<Int,Pair<Set<Int>, Set<Int>>>): Int {
         val cardCt = mutableMapOf<Int, Int>()
-        inp.keys.forEach { cardCt[it] = 1 }
-        inp.forEach{ (cardNo, data) ->
+        input.keys.forEach { cardCt[it] = 1 }
+        input.forEach{ (cardNo, data) ->
             val common = data.first.intersect(data.second)
             for(i in cardNo+1 until cardNo+1+common.size)
                 if(cardCt.keys.contains(i))
@@ -42,11 +38,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day04_test")
+    val testInput = parseInput(readInput("Day04_test"))
     check(part1(testInput) == 13)
     check(part2(testInput)== 30)
     measureTimeMillisPrint {
-        val input = readInput("Day04")
+        val input = parseInput(readInput("Day04"))
 
         println(part1(input))
         println(part2(input))
