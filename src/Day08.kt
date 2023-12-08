3dd2cf30ae7a2361fe11962774596d667f9565ce
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2023-12-08 12:49:25 +0100

    Day08 Part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,20 +1,48 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun part1(directions: String, input: Map<String, Pair<String, String>>): Long {
+        var curPos: String = input.keys.first()
+        var steps = 0L
+        while (true) {
+            for (ch in directions) {
+                val nPos = when {
+                    ch == 'L' -> input[curPos]!!.first
+                    ch == 'R' -> input[curPos]!!.second
+                    else -> error("Should never happen")
+                }
+
+                if (nPos == curPos) {
+                    error("Infinite loop")
+                }
+                curPos = nPos
+                steps++
+                if (curPos == "ZZZ")
+                    return steps
+
+            }
+        }
+    }
+
+    fun part2(directions: String, input: Map<String, Pair<String, String>>): Int {
         return 0
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun parseInput(input: List<String>): Pair<String, Map<String, Pair<String, String>>>{
+        input.take(2)
+        return Pair(input.first(), input.asSequence().drop(2).associate {
+            val key = it.split("=").first().trim()
+            val value = it.split("=").last().trim().removePrefix("(").removeSuffix(")").split(",")
+            key to Pair(value.first().trim(), value.last().trim())
+        })
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day08_test")
-    check(part1(testInput) == 0)
+    val testInput = parseInput(readInput("Day08_test"))
+    check(part1(testInput.first, testInput.second) == 2L)
     measureTimeMillisPrint {
-        val input = readInput("Day08")
+        val input = parseInput(readInput("Day08"))
 
-        println(part1(input))
-        println(part2(input))
+        println(part1(input.first, input.second))
+        println(part2(input.first, input.second))
     }
 }
