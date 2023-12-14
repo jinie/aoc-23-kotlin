7d3b19cf42a82dd95788fdb55ddfec85328fb94b
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2023-12-14 17:02:22 +0100

    checkpoint

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,5 +1,6 @@
 import utils.measureTimeMillisPrint
 import utils.readInput
+import kotlin.streams.toList
 
 fun main() {
 
@@ -7,7 +8,11 @@ fun main() {
         return this.map { StringBuilder(it) }.toMutableList()
     }
 
-    fun MutableList<StringBuilder>.moveRocks(direction:Int = -1): Long {
+    fun MutableList<StringBuilder>.transpose(): MutableList<StringBuilder>{
+        return this.map { it.chars().toList() }.transpose().map { StringBuilder(it.toString()) }.toMutableList()
+    }
+
+    fun MutableList<StringBuilder>.moveRocks(direction:Int = -1): MutableList<StringBuilder> {
         for(x in this[0].indices) {
             var done = false
             while(!done) {
@@ -28,23 +33,34 @@ fun main() {
                     done = true
             }
         }
-        return this.mapIndexed { index, row ->
-            row.count { it == 'O' } * (this.size - index)
-        }.sum().toLong()
-
+        return this
     }
 
     fun part1(input: List<String>): Long {
-        return input.parseInput().moveRocks()
+        return input.parseInput().moveRocks().mapIndexed { index, row ->
+            row.count { it == 'O' } * (input.size - index)
+        }.sum().toLong()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var rocks = input.parseInput()
+        for(i in 0 .. (1000000000/4)){
+            rocks = rocks.moveRocks(-1)
+            rocks = rocks.transpose()
+            rocks = rocks.moveRocks(-1)
+            rocks = rocks.transpose()
+            rocks = rocks.moveRocks(1)
+            rocks = rocks.transpose()
+            rocks = rocks.moveRocks(1)
+            rocks = rocks.transpose()
+        }
+        return rocks[0].count{ it == 'O'} * 10L
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14_test")
     check(part1(testInput) == 136L)
+    check(part2(testInput) == 64L)
     measureTimeMillisPrint {
         val input = readInput("Day14")
 
